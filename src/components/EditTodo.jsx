bb61809619b4be06785d1e34dbc3ca839828b6fb
M40 <modev.404@gmail.com>
2024-06-09 12:03:20 +0300

    Updated Edit

--- src/components/EditTodo.jsx
+++ src/components/EditTodo.jsx
@@ -1,7 +1,8 @@
 import React, { useRef } from 'react'
 import { useTodo } from '../hook/TodoHook';
+import { Check } from 'lucide-react';
 
-function EditTodo({id}) {
+function EditTodo({id,todo,setIsEditing}) {
     const {updateTodo} = useTodo()
     const ref  = useRef()
     const handleFormSubmit = (e)=>{
@@ -10,12 +11,13 @@ function EditTodo({id}) {
       const todo = formData.get(`${id}`)
       updateTodo(id,todo)
       ref.current.value = ""
+      setIsEditing(false)
     }
   
     return (
       <form onSubmit={handleFormSubmit} className='flex flex-row items-center'>
-          <input ref={ref} required type="text" name={`${id}`} className="bg-white placeholder-slate-400 rounded-l-md rounded-r-none focus:outline-none focus:border-indigo-500 border focus:ring-indigo-500 p-2" placeholder="Edit" />
-          <button className="bg-indigo-700 text-white shadow hover:bg-indigo-500  p-[0.58rem] rounded-l-none rounded-r-md">Edit</button>
+          <input defaultValue={todo} ref={ref} required type="text" name={`${id}`} className="bg-white placeholder-slate-400 rounded-l-md rounded-r-none focus:outline-none focus:border-indigo-500 border focus:ring-indigo-500 p-2" placeholder="Edit" />
+          <button className="bg-indigo-700 text-white shadow hover:bg-indigo-500  p-[0.58rem] rounded-l-none rounded-r-md"><Check/></button>
       </form>
     )
 }
